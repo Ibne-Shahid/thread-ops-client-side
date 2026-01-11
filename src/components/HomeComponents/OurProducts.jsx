e923bbe3718381a0402ca46ea447dbd712e5feb2
Ibne-Shahid <anasbinshahid2004@gmail.com>
2026-01-12 02:08:06 +0600

    products card modified

--- src/components/HomeComponents/OurProducts.jsx
+++ src/components/HomeComponents/OurProducts.jsx
@@ -27,7 +27,7 @@ const OurProducts = () => {
         transition={{ duration: 1 }} className="text-4xl text-center font-bold mb-10">Our Products</motion.h1>
 
       
-      <div className=" grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+      <div className=" grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center">
         {ourProducts.map((product) => <Card key={product._id} product={product}></Card>)}
       </div>
       <div className='text-center mt-15'>
